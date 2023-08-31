987bc1fb2d84303149bf8e166141e1097eaa411a
RomenJ <justiciaterapeutica@gmail.com>
2023-08-31 17:44:59 +0200

    commit 3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import  {Formik, Form, Field} from 'formik'
+import {useState} from 'react'
+import './header.css';
+import './content.css';
+import './article.css';
 
-function App() {
+const App =()=> {
+  const [photos,setPhotos]=useState([])
+  const open=url=> window.open(url)
+  console.log({photos})
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <div>
+   <header>
+    
+    <Formik
+    initialValues={{search: ''}} 
+    onSubmit={async values=>{
+//guad
+
+      const response= await fetch(`https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`,
+      {
+        headers: {
+          'Authorization': 'Client-ID M3dUd8-MVnhImxaf9yr-9j8-qrloncg_bjGy9qNv5JI'
+        }
+
+      })
+
+      const data= await response.json()
+      //llamar api unsplash
+ 
+     console.log('Valor de data',data.results)
+     setPhotos(data.results)
+     // console.log(values.search)
+    }}
+    >
+  
+      <Form>
+        <Field name="search" />
+      </Form>
+    </Formik>
+    
+   </header>
+
+
+      <div className='container'>
+        <div className='center'>
+
+          {photos.map (photo=>
+          <article key={photo.id} onClick={()=> open(photo.links.html)}>
+            <img src={photo.urls.regular} />
+            <p> {[photo.description, photo.alt_descriptiorn].join (' - ')}</p>
+          </article>
+
+
+          )}
+
+
+
+        </div>
+
+
+      </div>
+   </div> 
+  )
 }
 
 export default App;
